cbc523e7c57efdb936741ff984d5027b4f09eea4
Shruthika Uduthuri <suduthuri@lewer.com>
2026-01-22 15:03:10 -0600

    Basic UI

---
+++ policy-hub-frontend/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <span className="footer-copyright">&copy; All Rights Reserved</span>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ policy-hub-frontend/src/components/LoginButton/LoginButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoginButton.css';
+
+const LoginButton = () => {
+    return (
+        <button className="login-button">Login</button>
+    );
+};
+
+export default LoginButton;
